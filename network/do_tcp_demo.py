55b4ab8c4901277442f5748b696c8696dbbf7266
sheldon shen <d87335903@users.noreply.github.com>
2016-09-17 23:50:52 +0800

    using socket demonstrating tcp programming

---
+++ network/do_tcp_demo.py
@@ -0,0 +1,28 @@
+__author__ = 'shenyao'
+#客户端
+#导入socket库
+import socket
+#创建一个socket(AF_INET指定使用ipv4协议,SOCK_STREAM指定使用面向流的tcp协议)
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+#建立连接
+s.connect(('www.sina.com.cn',80))#connect方法参数是一个tuple
+#发送数据
+s.send(b'GET / HTTP/1.1\r\nHost: www.sina.com.cn\r\nConnection: close\r\n\r\n')
+#接收数据
+buffer=[]
+while True:
+    #每次最多接收1k字节
+    d=s.recv(1024)
+    if d:
+        buffer.append(d)
+    else:
+        break
+data=b''.join(buffer)
+#关闭连接
+s.close()
+#把HTTP头和网页分离一下
+header,html=data.split(b'\r\n\r\n',1)
+print(header.decode('utf-8'))
+#把接收的数据写入文件
+with open("sina.html",'wb') as f:
+    f.write(html)
